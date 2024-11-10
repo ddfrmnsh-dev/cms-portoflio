b319632b1c98f4d6a991fe6ff188e6d3752a0837
Dede Firmansah <dedefirmansah0508@gmail.com>
2024-11-10 18:00:28 +0700

    first commit

---
+++ src/components/users/UsersTable.jsx
@@ -0,0 +1,298 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { Search } from "lucide-react";
+import ReusableModal from "../common/ReusableModal";
+import { Button, Col, Form, Input, message, Row } from "antd";
+
+const userData = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "john@example.com",
+    role: "Customer",
+    status: "Active",
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    email: "jane@example.com",
+    role: "Admin",
+    status: "Active",
+  },
+  {
+    id: 3,
+    name: "Bob Johnson",
+    email: "bob@example.com",
+    role: "Customer",
+    status: "Inactive",
+  },
+  {
+    id: 4,
+    name: "Alice Brown",
+    email: "alice@example.com",
+    role: "Customer",
+    status: "Active",
+  },
+  {
+    id: 5,
+    name: "Charlie Wilson",
+    email: "charlie@example.com",
+    role: "Moderator",
+    status: "Active",
+  },
+];
+
+const UsersTable = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredUsers, setFilteredUsers] = useState(userData);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const [modalText, setModalText] = useState("Content of the modal");
+
+  const handleSearch = (e) => {
+    const term = e.target.value.toLowerCase();
+    setSearchTerm(term);
+    const filtered = userData.filter(
+      (user) =>
+        user.name.toLowerCase().includes(term) ||
+        user.email.toLowerCase().includes(term)
+    );
+    setFilteredUsers(filtered);
+  };
+
+  const showModal = () => {
+    setIsModalVisible(true);
+    console.log("true");
+  };
+
+  // Form submission handler
+  const onFinish = (values) => {
+    console.log("Form Submitted:", values);
+    setConfirmLoading(true);
+    setTimeout(() => {
+      setConfirmLoading(false);
+      setIsModalVisible(false); // Close modal after form submission
+      message.success("User added successfully!");
+    }, 2000);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+    message.error(errorInfo?.errorFields[0]?.errors);
+  };
+
+  const handleCancel = () => {
+    console.log("Cancel clicked");
+    setIsModalVisible(false);
+  };
+
+  return (
+    <motion.div
+      className="bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl p-6 border border-gray-700"
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.2 }}
+    >
+      <div className="flex justify-between items-center mb-6">
+        <h2 className="text-xl font-semibold text-gray-100">Users</h2>
+        <div className="relative">
+          <input
+            type="text"
+            placeholder="Search users..."
+            className="bg-gray-700 text-white placeholder-gray-400 rounded-lg pl-10 pr-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            value={searchTerm}
+            onChange={handleSearch}
+          />
+          <Search className="absolute left-3 top-2.5 text-gray-400" size={18} />
+        </div>
+      </div>
+
+      <div className="grid grid-rows-[auto,1fr,auto] overflow-x-auto h-full">
+        <button
+          className="bg-indigo-600 hover:bg-indigo-700 text-white font-normal py-2 px-3 mb-2 rounded transition duration-200 w-full sm:w-auto justify-self-end"
+          onClick={showModal}
+        >
+          Add User
+        </button>
+        <table className="min-w-full divide-y divide-gray-700">
+          <thead>
+            <tr>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
+                Name
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
+                Email
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
+                Role
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
+                Status
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
+                Actions
+              </th>
+            </tr>
+          </thead>
+
+          <tbody className="divide-y divide-gray-700">
+            {filteredUsers.map((user) => (
+              <motion.tr
+                key={user.id}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.3 }}
+              >
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <div className="flex items-center">
+                    <div className="flex-shrink-0 h-10 w-10">
+                      <div className="h-10 w-10 rounded-full bg-gradient-to-r from-purple-400 to-blue-500 flex items-center justify-center text-white font-semibold">
+                        {user.name.charAt(0)}
+                      </div>
+                    </div>
+                    <div className="ml-4">
+                      <div className="text-sm font-medium text-gray-100">
+                        {user.name}
+                      </div>
+                    </div>
+                  </div>
+                </td>
+
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <div className="text-sm text-gray-300">{user.email}</div>
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-blue-800 text-blue-100">
+                    {user.role}
+                  </span>
+                </td>
+
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <span
+                    className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${
+                      user.status === "Active"
+                        ? "bg-green-800 text-green-100"
+                        : "bg-red-800 text-red-100"
+                    }`}
+                  >
+                    {user.status}
+                  </span>
+                </td>
+
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-300">
+                  <button className="text-indigo-400 hover:text-indigo-300 mr-2">
+                    Edit
+                  </button>
+                  <button className="text-red-400 hover:text-red-300">
+                    Delete
+                  </button>
+                </td>
+              </motion.tr>
+            ))}
+          </tbody>
+        </table>
+
+        <ReusableModal
+          title="Add New User"
+          open={isModalVisible}
+          //   onOk={handleOk}
+          onCancel={handleCancel}
+          okText="Confirm"
+          cancelText="Dismiss"
+          confirmLoading={confirmLoading}
+        >
+          <div style={{ maxWidth: 600, margin: "0 auto" }}>
+            <Form
+              id="user-form" // Ensure this is the correct form ID
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+              layout="vertical"
+              colon={false}
+            >
+              <Row gutter={16}>
+                {/* Kolom pertama: Full Name dan Username */}
+                <Col span={12}>
+                  <Form.Item
+                    label="Full Name"
+                    name="fullName"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your full name!",
+                      },
+                    ]}
+                  >
+                    <Input placeholder="Enter your full name" />
+                  </Form.Item>
+                </Col>
+                <Col span={12}>
+                  <Form.Item
+                    label="Username"
+                    name="username"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your username!",
+                      },
+                    ]}
+                  >
+                    <Input placeholder="Enter your username" />
+                  </Form.Item>
+                </Col>
+              </Row>
+
+              <Row gutter={16}>
+                {/* Kolom kedua: Email dan Password */}
+                <Col span={12}>
+                  <Form.Item
+                    label="Email"
+                    name="email"
+                    rules={[
+                      { required: true, message: "Please input your email!" },
+                      { type: "email", message: "Please enter a valid email!" },
+                    ]}
+                  >
+                    <Input placeholder="Enter your email" />
+                  </Form.Item>
+                </Col>
+                <Col span={12}>
+                  <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your password!",
+                      },
+                    ]}
+                  >
+                    <Input.Password placeholder="Enter your password" />
+                  </Form.Item>
+                </Col>
+              </Row>
+
+              <div style={{ display: "flex", justifyContent: "flex-end" }}>
+                <Button onClick={handleCancel} style={{ marginRight: "8px" }}>
+                  Cancel
+                </Button>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  loading={confirmLoading}
+                  style={{
+                    backgroundColor: "#4f46e5",
+                    borderColor: "#4f46e5",
+                    color: "#ffffff",
+                  }}
+                >
+                  Submit
+                </Button>
+              </div>
+            </Form>
+          </div>
+        </ReusableModal>
+      </div>
+    </motion.div>
+  );
+};
+export default UsersTable;
